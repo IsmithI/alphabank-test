461f4575b032e5c7f01609089d37d8534d977f16
Oleg Bondar <olegbondar1998@gmail.com>
2018-11-29 09:10:31 +0200

    Beautifying albums

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -7,13 +7,14 @@ import Photo from './Photo';
 export default class Album extends Component {
 
 	render() {
-		const { title, topPhoto } = this.props.data;
+		const { title, topPhoto, photos } = this.props.data;
 		
 		return (
 			<div className="album card">
 				<Photo {...topPhoto} small/>
 				<div className="card-body">
 					<h5 className="card-title">{title}</h5>
+					<h6 className="card-subtitle mb-2 text-muted">Photos: {photos.length}</h6>
 				</div>
 			</div>
 		)
